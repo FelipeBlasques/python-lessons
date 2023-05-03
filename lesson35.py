02aec01bc4d4146a1d45b158c5e4acddf48d2cc0
Felipe Blasques <chomray@hotmail.com>
2023-05-03 16:56:33 -0300

    new lessons

---
+++ lesson35.py
@@ -0,0 +1,10 @@
+# string method
+
+string = 'felipe Blasques'
+other_var = f'{string[:3]}ABC{string[4: ]}'
+
+print(other_var)
+
+print(string.capitalize())
+print(string.isupper())
+print(string.zfill(100))
---
+++ lesson36.py
@@ -0,0 +1,14 @@
+# while
+
+condition = True
+
+while condition:
+    name = input(f'Whats your name: ')
+    print(f'Your name is {name}')
+
+    if name == 'Exit':
+        break
+
+print('End')
+    
+
